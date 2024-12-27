5e08a84ba662e7eabeb454529e390a148aa934d7
Lan Linh Nguyen <extonnguyen@gmail.com>
2024-12-28 02:11:36 +0700

    1-bootstrapping

---
+++ bootstrapping/src/main/java/com/example/bootstrapping/BootstrappingApplication.java
@@ -0,0 +1,21 @@
+package com.example.bootstrapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BootstrappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrappingApplication.class, args);
+    }
+
+    @GetMapping(value = "/")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
